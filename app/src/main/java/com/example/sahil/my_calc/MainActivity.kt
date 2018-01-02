a0194c4238635ceb09087da865ce1ba5b46d8035
Sahil Garg <sahilgarg8901@gmail.com>
2018-01-03 00:39:52 +0530

    Added Liberaries

--- app/src/main/java/com/example/sahil/my_calc/MainActivity.kt
+++ app/src/main/java/com/example/sahil/my_calc/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.sahil.my_calc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.graphics.BitmapFactory
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+
 
 class MainActivity : AppCompatActivity() {
 
