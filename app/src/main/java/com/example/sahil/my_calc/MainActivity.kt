eb83247ab5fa508f297b482440a4e78ab2806cf5
Sahil Garg <sahilgarg8901@gmail.com>
2018-01-16 01:18:37 +0530

    defined the empty onclick functions for all the buttons

--- app/src/main/java/com/example/sahil/my_calc/MainActivity.kt
+++ app/src/main/java/com/example/sahil/my_calc/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.sahil.my_calc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,63 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun ClearAllBtnClicked(view: View){
+
+    }
+    fun EraseOneBtnClicked(view: View){
+
+    }
+    fun DivideBtnClicked(view: View){
+
+    }
+    fun MultiplyBtnClicked(view: View){
+
+    }
+    fun SevenBtnClicked(view: View){
+
+    }
+    fun EightBtnClicked(view: View){
+
+    }
+    fun NineBtnClicked(view: View){
+
+    }
+    fun MinusBtnClicked(view: View){
+
+    }
+    fun FourBtnClicked(view: View){
+
+    }
+    fun FiveBtnClicked(view: View){
+
+    }
+    fun SixBtnClicked(view: View){
+
+    }
+    fun plusBtnClicked(view: View){
+
+    }
+    fun OneBtnClicked(view: View){
+
+    }
+    fun TwoBtnClicked(view: View){
+
+    }
+    fun ThreeBtnClicked(view: View){
+
+    }
+    fun EqualBtnClicked(view: View){
+
+    }
+    fun PercentBtnClicked(view: View){
+
+    }
+    fun ZeroBtnClicked(view: View){
+
+    }
+    fun DecimalBtnClicked(view: View){
+
+    }
+
 }
+
