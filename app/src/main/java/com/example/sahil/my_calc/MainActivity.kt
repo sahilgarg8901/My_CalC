3651743532ccbe0d8932a750db2744c0bdd63a7b
Sahil Garg <sahilgarg8901@gmail.com>
2018-01-29 22:31:53 +0530

    used clickListner instead of click functions

--- app/src/main/java/com/example/sahil/my_calc/MainActivity.kt
+++ app/src/main/java/com/example/sahil/my_calc/MainActivity.kt
@@ -22,7 +22,84 @@ class MainActivity : AppCompatActivity() {
         text = ""
         textView.text = ""
         //TextViewBar.setText("")
-    }
+        ClearAllButton.setOnClickListener{
+            textView.text = ""
+        }
+        DivideButton.setOnClickListener {
+            text = "${textView.text.toString()}/"
+            textView.text = text
+        }
+        MultiplyButton.setOnClickListener {
+                    text = "${textView.text.toString()}*"
+                    textView.text = text
+        }
+        SevenButton.setOnClickListener {
+                    text = "${textView.text.toString()}7"
+                    textView.text = text
+                }
+        EightButton.setOnClickListener {
+                    text = "${textView.text.toString()}8"
+                    textView.text = text
+                }
+        NineButton.setOnClickListener {
+                    text = "${textView.text.toString()}9"
+                    textView.text = text
+                }
+        MinusButton.setOnClickListener {
+                    text = "${textView.text.toString()}-"
+                    textView.text = text
+                }
+        FourButton.setOnClickListener {
+                    text = "${textView.text.toString()}4"
+                    textView.text = text
+        }
+        FiveButton.setOnClickListener {
+                    text = "${textView.text.toString()}5"
+                    textView.text = text
+        }
+        SixButton.setOnClickListener {
+                    text = "${textView.text.toString()}6"
+                    textView.text = text
+        }
+        plusButton.setOnClickListener {
+                    text = "${textView.text.toString()}+"
+                    textView.text = text
+        }
+        OneButton.setOnClickListener {
+                    text = "${textView.text.toString()}1"
+                    textView.text = text
+        }
+        TwoButton.setOnClickListener {
+                    text = "${textView.text.toString()}2"
+                    textView.text = text
+        }
+        ThreeButton.setOnClickListener {
+                    text = "${textView.text.toString()}3"
+                    textView.text = text
+        }
+        EqualButton.setOnClickListener {
+                    text = "${textView.text.toString()}="
+                    textView.text = text
+        }
+        PercentButton.setOnClickListener {
+                    text = "${textView.text.toString()}%"
+                    textView.text = text
+        }
+        ZeroButton.setOnClickListener {
+                    text = "${textView.text.toString()}0"
+                    textView.text = text
+        }
+        DecimalButton.setOnClickListener {
+                    text = "${textView.text.toString()}."
+                    textView.text = text
+        }
+        EraseOneButton.setOnClickListener {
+//                    text = "${textView.text.toString()}"
+//                    textView.text = text
+        }
+
+
+    }/*
     fun ClearAllBtnClicked(view: View){
 
         //TextViewBar.setText("")
@@ -118,6 +195,7 @@ class MainActivity : AppCompatActivity() {
         text = "${textView.text.toString()}="
         //TextViewBar.setText(text)
         textView.text = text
-    }
+    }*/
+
 }
 
